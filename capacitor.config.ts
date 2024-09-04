76047096a034d7727ee1bea6d83fa885b5cd8e3b
jgilman1337 <85707243+jgilman1337@users.noreply.github.com>
2024-09-04 00:24:13 -0700

    Upgrade deps and prep for WASM

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,8 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.stafford.vite.capacitor',
   appName: 'vite-and-capacitor',
-  webDir: 'dist',
-  bundledWebRuntime: false
+  webDir: 'dist'
+  //bundledWebRuntime: false
 };
 
 export default config;
