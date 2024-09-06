48c4c8df79f2ebdb358c65281cdbfb2c7ca4adc3
jgilman1337 <85707243+jgilman1337@users.noreply.github.com>
2024-09-05 18:50:56 -0700

    Serialize vaults to GOB streams before encryption

--- vaultlib_go/maindev/main.go
+++ vaultlib_go/maindev/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 
+	"wraith.me/vaultlib/vaultlib/util"
 	"wraith.me/vaultlib/vaultlib/vault"
 )
 
 func main(){
-	fmt.Println("heelo")
-	v := vault.Vault{}
-	//fmt.Printf("v = %v\n", v)
+	uuid := util.MustNewUUID7()
+	v := vault.New(uuid, "no_ident")
+
+	fmt.Printf("v = %v\n", v)
 	v.EncryptPassphrase("password")
 }
--- vaultlib_go/vaultlib/vault/ecrypt.go
+++ vaultlib_go/vaultlib/vault/ecrypt.go
@@ -1,6 +1,9 @@
 package vault
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/gob"
 	"fmt"
 
 	"wraith.me/vaultlib/vaultlib/crypto"
@@ -11,17 +14,36 @@ const _OPT_INFO = "__wraith-vaultlib-v1.0__"
 //Encrypts a vault using the passphrase method.
 func (v Vault) EncryptPassphrase(passphrase string) (*EVault, error){
 	//Derive a symmetric key via HKDF on the passphrase
-	key, err := crypto.HKDF([]byte(passphrase), nil, []byte(_OPT_INFO), crypto.PRIVKEY_SEED_SIZE)
+	keyb, err := crypto.HKDF([]byte(passphrase), nil, []byte(_OPT_INFO), crypto.PRIVKEY_SEED_SIZE)
 	if err != nil {
 		return nil, err
 	}
+	key := crypto.Privseed(keyb[:crypto.PRIVKEY_SEED_SIZE])
 
-	fmt.Println(key)
+	v.Encrypt(key)
 
 	return nil, nil
 }
 
 //Encrypts a vault object using XChaCha20-Poly1305, given a symmetric key.
 func (v Vault) Encrypt(key crypto.Privseed) (*EVault, error){
+	//Serialize the vault to a GOB stream
+	var vbytes bytes.Buffer
+	enc := gob.NewEncoder(&vbytes)
+	if err := enc.Encode(v); err != nil {
+		return nil, err
+	}
+
+	/*
+	aead, err := chacha20poly1305.NewX(key)
+    if err != nil {
+        log.Println("Error when creating cipher.")
+        panic(err)
+    }
+	*/
+
+	s := base64.StdEncoding.EncodeToString(vbytes.Bytes())
+	fmt.Printf("gob: %s\n", s)
+
 	return nil, nil
 }
