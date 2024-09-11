0fd8c9ec938a037d886eeffbb9d63f2353378d6f
ThomasN0712 <92189845+ThomasN0712@users.noreply.github.com>
2024-09-11 15:47:17 -0700

    bug fix

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 //import reactLogo from "../assets/react.svg";
-import "./App.scss";
+import "../../App.scss";
 import {IonButton, IonContent, IonInput, IonItem, IonPage} from "@ionic/react";
 
 //import "./App.scss";
