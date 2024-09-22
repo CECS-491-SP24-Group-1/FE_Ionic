2621c1a315c165a3ae313e22ca85d88b51172a41
mtkatsaros <mtkatsaros@gmail.com>
2024-09-22 12:26:46 -0700

    Use ionic camera API instead of browser API

--- src/pages/Camera.tsx
+++ src/pages/Camera.tsx
@@ -1,102 +1,57 @@
-import React, { useRef, useState, useEffect } from "react";
-import {
-	IonPage,
-	IonHeader,
-	IonToolbar,
-	IonTitle,
-	IonContent,
-	IonButton,
-	IonIcon
-} from "@ionic/react";
-import "./Camera.scss"; // Custom styling
+import React, { useState } from "react";
+import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
+import { IonIcon } from "@ionic/react";
+import { syncOutline, ellipseOutline, closeOutline } from "ionicons/icons";
+import "./Camera.scss";
 
-const Camera: React.FC = () => {
-	const videoRef = useRef<HTMLVideoElement>(null);
-	const canvasRef = useRef<HTMLCanvasElement>(null);
+const CameraPage: React.FC = () => {
 	const [photo, setPhoto] = useState<string | null>(null);
-	const [stream, setStream] = useState<MediaStream | null>(null);
 
-	// Function to start the camera stream
-	const startCamera = () => {
-		if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-			navigator.mediaDevices
-				.getUserMedia({ video: true })
-				.then((stream) => {
-					setStream(stream); // Store the stream in the state
-					if (videoRef.current) {
-						videoRef.current.srcObject = stream;
-					}
-				})
-				.catch((err) => {
-					console.error("Error accessing the camera: ", err);
-				});
+	const takePicture = async () => {
+		try {
+			const image = await Camera.getPhoto({
+				quality: 90,
+				allowEditing: false,
+				resultType: CameraResultType.Uri,
+				source: CameraSource.Camera
+			});
+
+			setPhoto(image.webPath || null);
+		} catch (error) {
+			console.error("Error taking picture: ", error);
 		}
 	};
 
-	// Request access to the user's camera when the component mounts
-	useEffect(() => {
-		startCamera();
-	}, []);
-
-	// Function to capture a photo from the video stream
-	const takePicture = () => {
-		const canvas = canvasRef.current;
-		const video = videoRef.current;
-		if (canvas && video) {
-			const context = canvas.getContext("2d");
-			if (context) {
-				// Set canvas size to match video size
-				canvas.width = video.videoWidth;
-				canvas.height = video.videoHeight;
-				// Draw the current video frame onto the canvas
-				context.drawImage(video, 0, 0, canvas.width, canvas.height);
-				// Get the data URL of the image
-				const imageData = canvas.toDataURL("image/png");
-				setPhoto(imageData);
-
-				// Stop the camera stream after taking the picture
-				if (stream) {
-					stream.getTracks().forEach((track) => track.stop());
-				}
-			}
-		}
-	};
-
-	// Function to reset the photo and restart the camera
 	const retakePicture = () => {
 		setPhoto(null);
-		startCamera(); // Restart the camera stream when retaking the picture
+	};
+
+	const toggleCamera = async () => {
+		// Capacitor Camera doesn't have a direct method to switch cameras
+		console.log("Nothing interesting happens.");
 	};
 
 	return (
-		<IonPage>
-			<IonHeader>
-				<IonToolbar>
-					<IonTitle>Camera</IonTitle>
-				</IonToolbar>
-			</IonHeader>
-			<IonContent className="ion-padding">
-				<div className="camera-container">
-					{/* Video element for displaying the camera stream */}
-					{!photo ? (
-						<video ref={videoRef} autoPlay playsInline></video>
-					) : (
-						<img src={photo} alt="Captured" />
-					)}
-
-					{/* Hidden canvas to capture the video frame */}
-					<canvas ref={canvasRef} style={{ display: "none" }}></canvas>
+		<div className="camera-container">
+			{photo ? (
+				<div className="photo-container">
+					<img src={photo} alt="Captured" className="photo" />
+					<button className="close-button" onClick={retakePicture}>
+						<IonIcon icon={closeOutline} />
+					</button>
 				</div>
-
-				{/* Button to take the picture */}
-				{!photo ? (
-					<div className="capture-button" onClick={takePicture}></div>
-				) : (
-					<div className="capture-button" onClick={retakePicture}></div>
-				)}
-			</IonContent>
-		</IonPage>
+			) : (
+				<div className="camera-view">
+					<button className="flip-button" onClick={toggleCamera}>
+						<IonIcon icon={syncOutline} />
+					</button>
+					<button className="snap-button" onClick={takePicture}>
+						<IonIcon icon={ellipseOutline} />
+					</button>
+				</div>
+			)}
+		</div>
 	);
 };
 
-export default Camera;
+export default CameraPage;
