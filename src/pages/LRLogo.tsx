dc0747e7afa39f9a8968ffab4f96d2d1d58eb331
jgilman1337 <85707243+jgilman1337@users.noreply.github.com>
2024-09-23 17:42:30 -0700

    Add favicons and resolve Vite compiler warnings

--- src/pages/LRLogo.tsx
+++ src/pages/LRLogo.tsx
@@ -1,8 +1,9 @@
 import logo from "../assets/images/glock_primary.svg";
+import "./LRLogo.scss";
 
 const LRLogo: React.FC = () => {
 	return (
-		<div className="logo-container">
+		<div className="logo-container flex flex-col items-center">
 			<img src={logo} alt="Wraith Logo" className="logo" />
 			<p className="logo-text">Wraith</p>
 		</div>
--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,36 @@ export default defineConfig({
 			registerType: "autoUpdate",
 			workbox: {
 				maximumFileSizeToCacheInBytes: 3000000 // Increased to ~3MB
+			},
+			manifest: {
+				name: "Wraith",
+				short_name: "Wraith",
+				start_url: "/",
+				display: "standalone",
+				background_color: "#ffffff",
+				theme_color: "#3367D6", // Add your desired theme color here
+				icons: [
+					{
+						src: "./public/favicon/favicon_64.png",
+						sizes: "64x64",
+						type: "image/png"
+					},
+					{
+						src: "./public/favicon/favicon_128.png",
+						sizes: "128x128",
+						type: "image/png"
+					},
+					{
+						src: "./public/favicon/favicon_256.png",
+						sizes: "256x256",
+						type: "image/png"
+					},
+					{
+						src: "./public/favicon/favicon_512.png",
+						sizes: "512x512",
+						type: "image/png"
+					}
+				]
 			}
 		})
 	],
@@ -22,7 +52,16 @@ export default defineConfig({
 		}
 	},
 	build: {
-		target: "esnext"
+		target: "esnext",
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					//Define your manual chunks here; this makes emitted JS files less heavyweight
+					vendor_react: ["react", "react-dom"],
+					vendor_ionic: ["@ionic/react", "@ionic/react-router"]
+				}
+			}
+		}
 	},
 	optimizeDeps: {
 		exclude: ["@vite/client", "@vite/env"]
