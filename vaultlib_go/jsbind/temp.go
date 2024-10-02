bfec9a98ccdd6576f17e0910c2f277301cfe2fd7
jgilman1337 <85707243+jgilman1337@users.noreply.github.com>
2024-10-01 23:38:21 -0700

    Add vault from keystore and minor API changes

--- vaultlib_go/jsbind/temp.go
+++ vaultlib_go/jsbind/temp.go
@@ -15,7 +15,7 @@ const HKDF_SALT = "0UsWeiCtpBO3N1sNnCyR/Q=="
 
 // HKDF(password: string): string
 func HKDF(this js.Value, args []js.Value) interface{} {
-	password := strings.TrimSpace(jsutil.Val2Any[string](args[0]))
+	password := strings.TrimSpace(jsutil.Cast[string](args[0]))
 	salt, _ := base64.StdEncoding.DecodeString(HKDF_SALT)
 	key, err := crypto.Ed25519Argon(password, salt)
 	if err != nil {
