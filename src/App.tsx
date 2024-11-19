7c87850b3c7d71e72f37ade678a18412e6c6c272
Thomas Nguyen <92189845+ThomasN0712@users.noreply.github.com>
2024-11-19 13:11:31 -0800

    adjust styling

--- src/App.tsx
+++ src/App.tsx
@@ -113,7 +113,7 @@ const App: React.FC = () => {
 					<div className="flex flex-col flex-1">
 						<IonTabs
 							className={`flex flex-col flex-1 translate-x-0 z-auto ${
-								isSidebarExpanded ? "sm:translate-x-64" : "sm:translate-x-20"
+								isSidebarExpanded ? "sm:translate-x-64 pr-64" : "sm:translate-x-20 pr-20"
 							}`}>
 							<IonRouterOutlet className="flex-1">
 								<>
