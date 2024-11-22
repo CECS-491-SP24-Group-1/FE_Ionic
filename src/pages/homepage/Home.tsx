98885914cbc2c16c2d3f79ab4864e027594898fa
Thomas Nguyen <92189845+ThomasN0712@users.noreply.github.com>
2024-11-22 00:40:21 -0800

    random

--- src/pages/homepage/Home.tsx
+++ src/pages/homepage/Home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { IonPage, IonContent } from "@ionic/react";
 import Hero from "./components/hero";
 import Features from "./components/features";
 import Technologies from "./components/technologies";
@@ -7,18 +8,27 @@ import { BackgroundBeams } from "@/components/background-beams";
 
 const Home: React.FC = () => {
 	return (
-		<div className="h-screen overflow-y-auto text-textPrimary dark:text-textPrimary-light">
-			{/* Hero Section */}
-			<Hero />
-			{/* Features Section */}
-			<Features />
-			{/* Technologies Section */}
-			<Technologies />
-			{/* Footer Section */}
-			<Footer />
-			{/* Background Beams */}
-			<BackgroundBeams className="bg-Primary z-[-100] dark:bg-primary-light" />
-		</div>
+		<IonPage>
+			<IonContent
+				className="h-screen overflow-y-auto bg-primary text-textPrimary dark:text-textPrimary-light"
+				scrollEvents={true} // Enables scroll tracking if needed
+			>
+				{/* Hero Section */}
+				<Hero />
+
+				{/* Features Section */}
+				<Features />
+
+				{/* Technologies Section */}
+				<Technologies />
+
+				{/* Footer Section */}
+				<Footer />
+
+				{/* Background Beams */}
+				<BackgroundBeams className="z-[-100] bg-transparent" />
+			</IonContent>
+		</IonPage>
 	);
 };
 
