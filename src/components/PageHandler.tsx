1a44fc72cc207461708c23b4f1f573c6669c624f
mtkatsaros <mtkatsaros@gmail.com>
2024-11-30 18:50:19 -0800

    Update pagination styling

--- src/components/PageHandler.tsx
+++ src/components/PageHandler.tsx
@@ -1,4 +1,4 @@
-import { IonButton, IonContent, IonIcon, IonInput, IonSelect, IonSelectOption, IonText } from "@ionic/react";
+import { IonButton, IonContent, IonIcon, IonInput, IonPage, IonSelect, IonSelectOption, IonText } from "@ionic/react";
 import { arrowBack, arrowForward, playSkipBack, playSkipForward } from "ionicons/icons";
 import React from "react";
 
@@ -43,72 +43,67 @@ const PageHandler:React.FC<PaginationDetails> = ({currentPage, setCurrentPage, t
     }
 
     return (
-        <div className="float-right flex items-center space-x-4">
+        <div className="flex items-center justify-center space-x-4 rounded-lg p-3">
             {/* First page button */}
             <IonButton
                 onClick={handleFirstPage}
                 disabled={currentPage === 1}
-                className="flex items-center justify-center bg-gray-300 hover:bg-gray-400 disabled:opacity-50 rounded-full p-2"
+                className="flex items-center justify-center bg-gray-700 hover:bg-gray-600 disabled:opacity-50 disabled:cursor-not-allowed rounded-full p-2"
             >
-                <IonIcon
-                icon={playSkipBack}
-                className="text-lg text-gray-800"
-                />
+                <IonIcon icon={playSkipBack} className="text-xl text-white" />
             </IonButton>
-    
+
             {/* Previous button */}
             <IonButton
                 onClick={handlePrevPage}
                 disabled={currentPage === 1}
-                className="flex items-center justify-center bg-gray-300 hover:bg-gray-400 disabled:opacity-50 rounded-full p-2"
+                className="flex items-center justify-center bg-gray-700 hover:bg-gray-600 disabled:opacity-50 disabled:cursor-not-allowed rounded-full p-2"
             >
-                <IonIcon
-                icon={arrowBack}
-                className="text-lg text-gray-800"
-                />
+                <IonIcon icon={arrowBack} className="text-xl text-white" />
             </IonButton>
 
             {/* Current page info */}
-            <IonText className="text-sm text-gray-600">
-                Page {currentPage} of {totalPages}
-            </IonText>
+            <div className="w-36 text-center">
+                <IonText className="text-sm text-gray-300">
+                    Page <span className="font-semibold">{currentPage}</span> of {" "}
+                    <span className="font-semibold">{perPage}</span>
+                </IonText>
+            </div>
 
             {/* Dropdown to Change Items Per Page */}
-            <IonSelect
-                value={perPage}
-                placeholder="Items per page"
-                onIonChange={(e) => handlePerPageChange(e.detail.value)}
-                className="text-sm text-gray-600 border border-gray-300 rounded"
-                interface="popover"
-            >
-                <IonSelectOption value={5}>5</IonSelectOption>
-                <IonSelectOption value={10}>10</IonSelectOption>
-                <IonSelectOption value={20}>20</IonSelectOption>
-                <IonSelectOption value={50}>50</IonSelectOption>
-            </IonSelect>
+            <>
+                <IonText className="text-sm text-gray-300">page items: </IonText>
+                <IonSelect
+                    value={perPage}
+                    placeholder="Items per page"
+                    onIonChange={(e) => handlePerPageChange(e.detail.value)}
+                    className="text-sm text-gray-300 bg-gray-700 border border-gray-600 rounded-lg"
+                    interface="popover"
+                >
+                    <IonSelectOption value={5}>5</IonSelectOption>
+                    <IonSelectOption value={10}>10</IonSelectOption>
+                    <IonSelectOption value={20}>20</IonSelectOption>
+                    <IonSelectOption value={50}>50</IonSelectOption>
+                </IonSelect>
+            </>
+            
 
             {/* Next button */}
             <IonButton
                 onClick={handleNextPage}
                 disabled={currentPage === totalPages}
-                className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 disabled:opacity-50 rounded-full p-2"
+                className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed rounded-full p-2"
             >
-                <IonIcon
-                icon={arrowForward}
-                className="text-lg text-white"
-                />
+                <IonIcon icon={arrowForward} className="text-xl text-white" />
             </IonButton>
 
             {/* Last page button */}
             <IonButton
                 onClick={handleLastPage}
                 disabled={currentPage === totalPages}
-                className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 disabled:opacity-50 rounded-full p-2"
+                className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed rounded-full p-2"
             >
-                <IonIcon
-                icon={playSkipForward}
-                className="text-lg text-white"
-                />
+                <IonIcon icon={playSkipForward} className="text-xl text-white" />
             </IonButton>
         </div>
     )
